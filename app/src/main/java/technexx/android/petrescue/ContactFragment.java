4103aa291929df6bd4fe63cf4ea79f31846cc612
technexx <48839830+technexx@users.noreply.github.com>
2020-06-11 14:26:52 -0700

    redid contact layout

--- app/src/main/java/technexx/android/petrescue/ContactFragment.java
+++ app/src/main/java/technexx/android/petrescue/ContactFragment.java
@@ -15,6 +15,10 @@ public class ContactFragment extends Fragment {
         View root = inflater.inflate(R.layout.contact_fragment, container, false);
 
         TextView contact_one = root.findViewById(R.id.contact_one);
+        TextView contact_two = root.findViewById(R.id.contact_two);
+        TextView contact_three = root.findViewById(R.id.contact_three);
+        TextView contact_four = root.findViewById(R.id.contact_four);
+
 
         Bundle args = getArguments();
         String shelter = null;
@@ -24,17 +28,24 @@ public class ContactFragment extends Fragment {
         }
 
         if (shelter.equals("ev")) {
-            contact_one.setText(getString(R.string.eastValley_contact));
+            contact_one.setText(getString(R.string.loc_eastValley));
+            contact_two.setText(getString(R.string.eastValley_address));
         }
         if (shelter.equals("wLA")) {
-            contact_one.setText(getString(R.string.westLA_contact));
+            contact_one.setText(getString(R.string.loc_westLA));
+            contact_two.setText(getString(R.string.westLA_address));
         }
         if (shelter.equals("sLA")) {
-            contact_one.setText(getString(R.string.southLA_contact));
+            contact_one.setText(getString(R.string.loc_southLA));
+            contact_two.setText(getString(R.string.southLA_address));
         }
         if (shelter.equals("har")) {
-            contact_one.setText(getString(R.string.harbor_contact));
+            contact_one.setText(getString(R.string.loc_harbor));
+            contact_two.setText(getString(R.string.harbor_address));
         }
+
+        contact_three.setText(getString(R.string.shelter_hours));
+        contact_four.setText(getString(R.string.shelter_phone));
 
         return root;
     }
