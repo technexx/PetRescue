6a73e42b77c8110003758e351fdcf6b21a9c0e58
technexx <48839830+technexx@users.noreply.github.com>
2020-06-11 15:01:54 -0700

    added phone # link

--- app/src/main/java/technexx/android/petrescue/ContactFragment.java
+++ app/src/main/java/technexx/android/petrescue/ContactFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,6 +25,7 @@ public class ContactFragment extends Fragment {
         TextView contact_three = root.findViewById(R.id.contact_three);
         TextView contact_four = root.findViewById(R.id.contact_four);
 
+        //Setting underline on address.
         contact_two.setPaintFlags(contact_two.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
 
         Bundle args = getArguments();
@@ -57,15 +59,20 @@ public class ContactFragment extends Fragment {
         contact_three.setText(getString(R.string.shelter_hours));
         contact_four.setText(getString(R.string.shelter_phone));
 
+        //Linkify makes the Uri coordinates unnecessary, but we're keeping them as a backup.
+        Linkify.addLinks(contact_two, Linkify.ALL);
+        Linkify.addLinks(contact_four, Linkify.ALL);
+        contact_two.setLinkTextColor(getResources().getColor(R.color.darkGreen));
+        contact_four.setLinkTextColor(getResources().getColor(R.color.darkGreen));
 
-        contact_two.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_VIEW, address);
-                intent.setPackage("com.google.android.apps.maps");
-                startActivity(intent);
-            }
-        });
+//        contact_two.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(Intent.ACTION_VIEW, address);
+//                intent.setPackage("com.google.android.apps.maps");
+//                startActivity(intent);
+//            }
+//        });
 
         return root;
     }
