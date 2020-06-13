7f5c3f453dccea6c82bca154c01bf255d876a865
technexx <48839830+technexx@users.noreply.github.com>
2020-06-13 15:33:55 -0700

    Added popup

--- app/src/main/java/technexx/android/petrescue/MenuFragment.java
+++ app/src/main/java/technexx/android/petrescue/MenuFragment.java
@@ -1,19 +1,29 @@
 package technexx.android.petrescue;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+
+import java.util.zip.Inflater;
 
 public class MenuFragment extends Fragment {
 
@@ -61,6 +71,32 @@ public class MenuFragment extends Fragment {
 
         editor.putString("animal", "");
         editor.apply();
+
+        final ConstraintLayout menuFragment = root.findViewById(R.id.menu_constrained);
+
+        LayoutInflater layoutInflater = (LayoutInflater) getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View myView = layoutInflater.inflate(R.layout.disclaimer, null);
+
+//       final PopupWindow popupWindow = new PopupWindow(myView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        final PopupWindow popupWindow = new PopupWindow(myView, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        //This runnable is necessary to prevent the popup from inflating too early and crashing the app.
+        menuFragment.post(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    popupWindow.setWidth(800);
+                    popupWindow.setHeight(800);
+                    popupWindow.setFocusable(true);
+                    popupWindow.setElevation(5);
+                    popupWindow.showAtLocation(menuFragment, Gravity.CENTER, 0, 0);
+                } catch (Exception e) {
+                    Log.e("Error", "Popup trying to attach too early");
+                }
+            }
+        });
 
         ImageButton dogs = root.findViewById(R.id.dogs);
         ImageButton cats = root.findViewById(R.id.cats);
